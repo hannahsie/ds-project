4554373c4940e02298b8660f2b7ed4c37f6d94c1
hannah.maret.siemsen <hannah.maret.siemsen@stud.uni-hannover.de>
2024-11-14 17:03:55 +0100

    final

--- src/de/luh/vss/chat/client/ChatClient.java
+++ src/de/luh/vss/chat/client/ChatClient.java
@@ -2,8 +2,12 @@ package de.luh.vss.chat.client;
 
 import java.io.IOException;
 
-public class ChatClient {
+import de.luh.vss.chat.common.Message;
+import de.luh.vss.chat.common.User;
+import de.luh.vss.chat.common.Message.ChatMessage;
+import de.luh.vss.chat.common.User.UserId;
 
+public class ChatClient {
 	public static void main(String... args) {
 		try {
 			new ChatClient().start();
@@ -16,6 +20,10 @@ public class ChatClient {
 		System.out.println("Congratulation for successfully setting up your environment for Assignment 1!");
 		
 		// implement your chat client logic here
+		UserId id = new UserId(7262);
+		
+		ChatMessage mess = new ChatMessage(id, "TEST 1 USER ID CORRECTNESS");
+		System.out.println(mess.toString());
 	}
 
 }
